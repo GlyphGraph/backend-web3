5af767d0899baccb9534d876cba571a6689f048e
notRyuk <amadeusrobot.notryuk@gmail.com>
2024-04-06 12:45:18 +0530

    testing needs to be deployed

--- contracts/GlyphGraph.sol
+++ contracts/GlyphGraph.sol
@@ -8,76 +8,147 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 // testing purposes only
 import "hardhat/console.sol";
 
-contract GlyphGraph is ERC721URIStorage {
-    struct User {
-        string _id;
-        address _address;
-        string email;
-        string name;
-        string password;
-        uint256 timestamp;
-        bool registered;
-    }
-    mapping(address => User) private Users;
-    event UserCreated(
-        string _id,
-        string email,
-        string name,
-        string password,
-        uint256 timestamp
-    );
+struct User {
+    string _id;
+    address _address;
+    string email;
+    string name;
+    string password;
+    uint256 timestamp;
+    bool registered;
+}
 
-    struct Vault {
-        string _id;
-        address user;
-        string name;
-        bool protected;
-        string password;
-        bool created;
-    }
-    mapping(string => Vault) private Vaults;
-    event VaultCreated(
-        string _id,
-        address user,
-        string nam,
-        bool protecte,
-        string password
-    );
+struct Vault {
+    string _id;
+    address user;
+    string name;
+    bool protected;
+    string password;
+    bool created;
+}
 
-    struct Password {
-        string image;
-        uint32 size;
-        uint32 row;
-        uint32 col;
-        bool specialCharacters;
-        bool capitalLetters;
-        bool includeNumbers;
-        address user;
-        bool created;
-    }
-    mapping(string => Password) private Passwords;
-    event PasswordCreated(Password password);
+enum PasswordType {
+    RANDOM,
+    MEMORABLE
+}
 
-    struct Login {
-        Vault vault;
-        string title;
-        string username;
-        Password password;
-        string[] websites;
-        string note;
-        bool created;
-    }
-    mapping(string => Login) private Logins;
-    event LoginCreated(Login login);
+struct Password {
+    address user;
+    string image;
+    uint32 rows;
+    uint32 cols;
+    bool specialCharacters;
+    bool capitalLetters;
+    bool numbers;
+    PasswordType passwordType;
+    bool created;
+}
 
+struct Login {
+    Vault vault;
+    string title;
+    string username;
+    Password password;
+    string[] websites;
+    string note;
+    bool created;
+}
+
+interface IGlyphGraph is IERC721 {
+    // user
+    function getUserDetails() external view returns (User memory);
+
+    function addUser(
+        string memory _email,
+        string memory _name,
+        string memory _password
+    ) external;
+
+    function updateUserPassword(string memory _password) external;
+
+    // vault
+    function createVault(string memory _name) external;
+
+    function createVaultWithPassword(
+        string memory _name,
+        string memory _password
+    ) external;
+
+    function getAllVaults() external view returns (Vault[] memory);
+
+    function updateVaultName(
+        string memory _vaultId,
+        string memory _newName
+    ) external;
+
+    function updateVaultPassword(
+        string memory _vaultId,
+        string memory _newPassword
+    ) external;
+
+    function protectVault(
+        string memory _vaultId,
+        string memory _newPassword
+    ) external;
+
+    function removeProtection(string memory _vaultId) external;
+
+    // password
+    function createPassword(
+        string memory _image,
+        uint32 _rows,
+        uint32 _cols,
+        PasswordType _passwordType
+    ) external;
+
+    function createPasswordWithSettings(
+        string memory _image,
+        uint32 _rows,
+        uint32 _cols,
+        bool _specialCharacters,
+        bool _capitalLetters,
+        bool _numbers,
+        PasswordType _passwordType
+    ) external;
+
+    function getPassword(
+        string memory _id
+    ) external view returns (Password memory);
+
+    function getUserPasswords() external view returns (Password[] memory);
+
+    // logins
+    function createLogin(
+        string memory _vaultId,
+        string memory _title,
+        string memory _username,
+        string memory _passwordId,
+        string[] memory _websites,
+        string memory _note
+    ) external;
+
+    function getLogin(string memory _id) external view returns (Login memory);
+    function getUserLogins() external view returns (Login[] memory);
+    function getVaultLogins(string memory _vaultId) external view returns (Login[] memory);
+}
+
+contract GlyphGraph is ERC721URIStorage, IGlyphGraph {
     error GlyphGraph__UserAlreadyExists();
-    error GlyphGraph__UserDoesNotFound();
+    error GlyphGraph__UserNotFound();
     error GlyphGraph__VaultNotFound();
     error GlyphGraph__VaultAlreadyExists();
     error GlyphGraph__PasswordNotFound();
     error GlyphGraph__PasswordAlreadyExists();
     error GlyphGraph__LoginAlreadyExists();
     error GlyphGraph__LoginNotFound();
+
+    mapping(address => User) private Users;
+    mapping(address => string[]) private UserVaults;
+    mapping(string => Vault) private Vaults;
+    mapping(address => string[]) private UserPasswords;
+    mapping(string => Password) private Passwords;
+    mapping(address => string[]) private UserLogins;
+    mapping(string => Login) private Logins;
 
     function __generateRandomString(
         uint256 length
@@ -99,58 +170,311 @@ contract GlyphGraph is ERC721URIStorage {
 
     constructor() ERC721("GlyphGraphVaults", "GLG") {}
 
-    function createUser(
+    function addUser(
         string memory _name,
         string memory _email,
         string memory _password
-    ) publi {
-        if (Users[msg.sender].registered == true) {
-            revert GlyphGraph__UserAlreadyExists(); 
+    ) public {
+        if (
+            Users[msg.sender].registered == true ||
+            bytes(Users[msg.sender]._id).length > 0
+        ) {
+            revert GlyphGraph__UserAlreadyExists();
         }
         string memory _randomId = __generateRandomString(32);
-        Users[msg.sender]._id = _randomId;
-        Users[msg.sender]._address = msg.sender;
-        Users[msg.sender].name = _name;
-        Users[msg.sender].email = _email;
+        Users[msg.sender] = User(
+            _randomId,
+            msg.sender,
+            _name,
+            _email,
+            _password,
+            block.timestamp,
+            true
+        );
+    }
+
+    function getUserDetails() public view returns (User memory) {
+        User memory __user = Users[msg.sender];
+        if (bytes(__user._id).length == 0) {
+            revert GlyphGraph__UserNotFound();
+        }
+        return __user;
+    }
+
+    function updateUserPassword(string memory _password) public {
+        User memory __user = Users[msg.sender];
+        if (bytes(__user._id).length == 0) {
+            revert GlyphGraph__UserNotFound();
+        }
         Users[msg.sender].password = _password;
-        Users[msg.sender].timestamp = block.timestamp;
-        emit UserCreated(_randomId, _email, _name, _password, block.timestamp);
-        // _mint(msg.sender, 1);
     }
 
-    function getUser(address _address) external view returns (User memory) {
-        return Users[_address];
-    }
-
-    function createVault(address _user, string memory _name) public {
-        User memory __user = Users[_user];
-        require(bytes(__user._id).length > 0, "User not found");
+    function createVault(string memory _name) public {
+        User memory __user = Users[msg.sender];
+        if (bytes(__user._id).length == 0) {
+            revert GlyphGraph__UserNotFound();
+        }
         string memory _randomId = __generateRandomString(32);
-        Vault memory _vault = Vault({
-            _id: _randomId,
-            user: _user,
-            name: _name,
-            protected: false,
-            password: "",
-            created: true
-        });
+        Vault memory _vault = Vault(
+            _randomId,
+            msg.sender,
+            _name,
+            false,
+            "",
+            true
+        );
         Vaults[_randomId] = _vault;
-        emit VaultCreated(_randomId, msg.sender, _name, false, "");
+        UserVaults[msg.sender].push(_randomId);
     }
 
-    function createVaultWithPassword(address _user, string memory _name, string memory _password) public {
-        User memory __user = Users[_user];
-        require(bytes(__user._id).length > 0, "User not found");
+    function createVaultWithPassword(
+        string memory _name,
+        string memory _password
+    ) public {
+        User memory __user = Users[msg.sender];
+        if (bytes(__user._id).length == 0) {
+            revert GlyphGraph__UserNotFound();
+        }
         string memory _randomId = __generateRandomString(32);
-        Vault memory _vault = Vault({
-            _id: _randomId,
-            user: _user,
-            name: _name,
-            protected: true,
-            password: _password,
-            created: true
-        });
+        Vault memory _vault = Vault(
+            _randomId,
+            msg.sender,
+            _name,
+            true,
+            _password,
+            true
+        );
         Vaults[_randomId] = _vault;
-        emit VaultCreated(_randomId, msg.sender, _name, true, _password);
+        UserVaults[msg.sender].push(_randomId);
+    }
+
+    function getAllVaults() public view returns (Vault[] memory) {
+        User memory __user = Users[msg.sender];
+        if (bytes(__user._id).length == 0) {
+            revert GlyphGraph__UserNotFound();
+        }
+        Vault[] memory _vaults = new Vault[](UserVaults[msg.sender].length);
+        for (uint256 i = 0; i < UserVaults[msg.sender].length; i++) {
+            string memory vaultId = UserVaults[msg.sender][i];
+            _vaults[i] = Vaults[vaultId];
+        }
+        return _vaults;
+    }
+
+    function updateVaultName(
+        string memory _vaultId,
+        string memory _newName
+    ) public {
+        User memory __user = Users[msg.sender];
+        if (bytes(__user._id).length == 0) {
+            revert GlyphGraph__UserNotFound();
+        }
+        if (bytes(Vaults[_vaultId]._id).length == 0) {
+            revert GlyphGraph__VaultNotFound();
+        }
+        Vaults[_vaultId].name = _newName;
+    }
+
+    function updateVaultPassword(
+        string memory _vaultId,
+        string memory _newPassword
+    ) public {
+        User memory __user = Users[msg.sender];
+        if (bytes(__user._id).length == 0) {
+            revert GlyphGraph__UserNotFound();
+        }
+        if (bytes(Vaults[_vaultId]._id).length == 0) {
+            revert GlyphGraph__VaultNotFound();
+        }
+        Vaults[_vaultId].password = _newPassword;
+    }
+
+    function protectVault(
+        string memory _vaultId,
+        string memory _newPassword
+    ) public {
+        User memory __user = Users[msg.sender];
+        if (bytes(__user._id).length == 0) {
+            revert GlyphGraph__UserNotFound();
+        }
+        if (bytes(Vaults[_vaultId]._id).length == 0) {
+            revert GlyphGraph__VaultNotFound();
+        }
+        Vaults[_vaultId].protected = true;
+        Vaults[_vaultId].password = _newPassword;
+    }
+
+    function removeProtection(string memory _vaultId) public {
+        User memory __user = Users[msg.sender];
+        if (bytes(__user._id).length == 0) {
+            revert GlyphGraph__UserNotFound();
+        }
+        if (bytes(Vaults[_vaultId]._id).length == 0) {
+            revert GlyphGraph__VaultNotFound();
+        }
+        Vaults[_vaultId].protected = false;
+        Vaults[_vaultId].password = "";
+    }
+
+    function createPassword(
+        string memory _image,
+        uint32 _rows,
+        uint32 _cols,
+        PasswordType _passwordType
+    ) external {
+        User memory __user = Users[msg.sender];
+        if (bytes(__user._id).length == 0) {
+            revert GlyphGraph__UserNotFound();
+        }
+        string memory _randomId = __generateRandomString(32);
+        Password memory _password = Password(
+            msg.sender,
+            _image,
+            _rows,
+            _cols,
+            true,
+            true,
+            true,
+            _passwordType,
+            true
+        );
+        Passwords[_randomId] = _password;
+        UserPasswords[msg.sender].push(_randomId);
+    }
+
+    function createPasswordWithSettings(
+        string memory _image,
+        uint32 _rows,
+        uint32 _cols,
+        bool _specialCharacters,
+        bool _capitalLetters,
+        bool _numbers,
+        PasswordType _passwordType
+    ) external {
+        User memory __user = Users[msg.sender];
+        if (bytes(__user._id).length == 0) {
+            revert GlyphGraph__UserNotFound();
+        }
+        string memory _randomId = __generateRandomString(32);
+        Password memory _password = Password(
+            msg.sender,
+            _image,
+            _rows,
+            _cols,
+            _specialCharacters,
+            _capitalLetters,
+            _numbers,
+            _passwordType,
+            true
+        );
+        Passwords[_randomId] = _password;
+        UserPasswords[msg.sender].push(_randomId);
+    }
+
+    function getPassword(
+        string memory _id
+    ) external view returns (Password memory) {
+        User memory __user = Users[msg.sender];
+        if (bytes(__user._id).length == 0) {
+            revert GlyphGraph__UserNotFound();
+        }
+        if (bytes(Passwords[_id].image).length == 0) {
+            revert GlyphGraph__PasswordNotFound();
+        }
+        return Passwords[_id];
+    }
+
+    function getUserPasswords() external view returns (Password[] memory) {
+        User memory __user = Users[msg.sender];
+        if (bytes(__user._id).length == 0) {
+            revert GlyphGraph__UserNotFound();
+        }
+
+        Password[] memory userPasswords = new Password[](UserPasswords[msg.sender].length);
+        for (uint256 i = 0; i < UserPasswords[msg.sender].length; i++) {
+            string memory passwordId = UserPasswords[msg.sender][i];
+            userPasswords[i] = Passwords[passwordId];
+        }
+        return userPasswords;
+    }
+
+    function createLogin(
+        string memory _vaultId,
+        string memory _title,
+        string memory _username,
+        string memory _passwordId,
+        string[] memory _websites,
+        string memory _note
+    ) external {
+        User memory __user = Users[msg.sender];
+        if (bytes(__user._id).length == 0) {
+            revert GlyphGraph__UserNotFound();
+        }
+        if (bytes(Vaults[_vaultId]._id).length == 0) {
+            revert GlyphGraph__VaultNotFound();
+        }
+        if (bytes(Passwords[_passwordId].image).length == 0) {
+            revert GlyphGraph__PasswordNotFound();
+        }
+        string memory _randomId = __generateRandomString(32);
+        Login memory _login = Login(
+            Vaults[_vaultId],
+            _title,
+            _username,
+            Passwords[_passwordId],
+            _websites,
+            _note,
+            true
+        );
+        Logins[_randomId] = _login;
+        UserLogins[msg.sender].push(_randomId);
+    }
+
+    function getLogin(string memory _id) external view returns (Login memory) {
+        User memory __user = Users[msg.sender];
+        if (bytes(__user._id).length == 0) {
+            revert GlyphGraph__UserNotFound();
+        }
+        if (bytes(Logins[_id].vault._id).length == 0) {
+            revert GlyphGraph__LoginNotFound();
+        }
+        return Logins[_id];
+    }
+
+    function getUserLogins() external view returns (Login[] memory) {
+        User memory __user = Users[msg.sender];
+        if (bytes(__user._id).length == 0) {
+            revert GlyphGraph__UserNotFound();
+        }
+
+        Login[] memory userLogins = new Login[](UserLogins[msg.sender].length);
+        for (uint256 i = 0; i < UserLogins[msg.sender].length; i++) {
+            string memory loginId = UserLogins[msg.sender][i];
+            userLogins[i] = Logins[loginId];
+        }
+        return userLogins;
+    }
+
+    function getVaultLogins(string memory _vaultId) external view returns (Login[] memory) {
+        User memory __user = Users[msg.sender];
+        if (bytes(__user._id).length == 0) {
+            revert GlyphGraph__UserNotFound();
+        }
+
+        if (bytes(Vaults[_vaultId]._id).length == 0) {
+            revert GlyphGraph__VaultNotFound();
+        }
+
+        Login[] memory vaultLogins = new Login[](UserLogins[msg.sender].length);
+        uint256 counter = 0;
+        for (uint256 i = 0; i < UserLogins[msg.sender].length; i++) {
+            string memory loginId = UserLogins[msg.sender][i];
+            Login memory login = Logins[loginId];
+            if (Strings.equal(login.vault._id, _vaultId)) {
+                vaultLogins[counter] = login;
+                counter++;
+            }
+        }
+        return vaultLogins;
     }
 }
--- test/GlphGraph.ts
+++ test/GlphGraph.ts
@@ -1,59 +1,62 @@
 import {
-  loadFixture
+    loadFixture
 } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { assert } from "chai";
 import hre from "hardhat";
 
 describe("GlyphGraph", function () {
-  async function deployContract() {
-    const [owner, otherAccount] = await hre.ethers.getSigners();
-    const Lock = await hre.ethers.getContractFactory("GlyphGraph");
-    const gg = await Lock.deploy();
-    console.log("owner", owner.address)
-    return { gg, owner, otherAccount };
-  }
+    enum PasswordType {
+        RANDOM,
+        MEMORABLE
+    }
 
-  describe("Deployment", function () {
-    it("The contract should be deployed", async function () {
-      const { gg, } = await loadFixture(deployContract);
-      assert(gg !== null)
-    })
-  });
+    async function deployContract() {
+        const [owner, otherAccount] = await hre.ethers.getSigners();
+        const Lock = await hre.ethers.getContractFactory("GlyphGraph");
+        const gg = await Lock.deploy();
+        console.log("owner", owner.address)
+        return { gg, owner, otherAccount };
+    }
 
-  describe("User", () => {
-    it("The user1 should be created", async () => {
-      const { gg } = await loadFixture(deployContract)
-      const { data } = await gg.createUser("Name", "email@gmail.com", "123")
-      console.log(data)
-      assert(data !== null)
+    async function createUser() {
+        const contract = await deployContract()
+        await contract.gg.addUser("Name", "email@gmail.com", "123")
+        return contract;
+    }
+
+    describe("Deployment", function () {
+        it("The contract should be deployed", async function () {
+            const { gg, } = await loadFixture(deployContract);
+            assert(gg !== null)
+        })
+    });
+
+    describe("User", () => {
+        it("The user1 should be created", async () => {
+            const { gg } = await loadFixture(deployContract)
+            const { data } = await gg.addUser("Name", "email@gmail.com", "123")
+            const user = await gg.getUserDetails();
+            console.log(user)
+            assert(user !== null)
+        })
+
+        it("The user2 should be created", async () => {
+            const { gg } = await loadFixture(deployContract)
+            const { data } = await gg.addUser("Name", "email1@gmail.com", "123")
+            const user = await gg.getUserDetails();
+            console.log(user)
+            assert(user !== null)
+        })
     })
 
-    it("The user2 should be created", async () => {
-      const { gg } = await loadFixture(deployContract)
-      const { data } = await gg.createUser("Name", "email1@gmail.com", "123")
-      console.log(data)
-      assert(data !== null)
+    describe("Vault", () => {
+        it("The vault1 should be created", async () => {
+            const { gg, owner } = await loadFixture(createUser)
+            await gg.createVault("v1")
+            await gg.createVaultWithPassword("v2", "password123")
+            const data = await gg.getAllVaults()
+            console.log(data)
+            assert(data !== null)
+        })
     })
-
-    it("Get the user",  async () => {
-      const { gg, owner } = await loadFixture(deployContract)
-      const user = await gg.getUser(owner.address);
-      console.log(user)
-      assert(user !== null)
-    })
-  })
-
-  describe("Vault", () => {
-    it("The vault1 should be created", async () => {
-      const { gg, owner } = await loadFixture(deployContract)
-      const { data } = await gg.createVault(owner.address, "v1")
-      assert(data !== null)
-    })
-
-    it("The vault2 should be created", async () => {
-      const { gg, owner } = await loadFixture(deployContract)
-      const { data } = await gg.createVaultWithPassword(owner.address, "v1", "password1")
-      assert(data !== null)
-    })
-  })
 });
